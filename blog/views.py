ecfdd228f01e9da207bd800af296d67547edbe83
Pedro Miguel <pedro.miguel@live.co.uk>
2017-08-03 11:51:58 +0100

    Adds post views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context {'posts': posts}
+
+    return render(request, 'blog/post/list.html', context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             published_date__year=year,
+                             published_date__month=month,
+                             published_date__day=day)
+    context = {'post': post}
+
+    return render(request, 'blog/post/detail.html', context)
