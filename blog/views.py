90a52dc6ee53a271c6e56154a211b62ee601ec93
Pedro Miguel <pedro.miguel@live.co.uk>
2017-08-03 12:51:21 +0100

    Adds pagination, updates post_list to use class based views

--- blog/views.py
+++ blog/views.py
@@ -2,15 +2,36 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
-    context {'posts': posts}
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3)  # 3 posts per page
+    page = request.GET.get('page')
 
-    return render(request, 'blog/post/list.html', context)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+
+    context = {'page': page, 'posts': posts}
+
+    return render(request, 'blog/post/post_list.html', context)
+
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/post_list.html'
 
 
 def post_detail(request, year, month, day, post):
@@ -22,4 +43,4 @@ def post_detail(request, year, month, day, post):
                              published_date__day=day)
     context = {'post': post}
 
-    return render(request, 'blog/post/detail.html', context)
+    return render(request, 'blog/post/post_detail.html', context)
