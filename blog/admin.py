9209629b732036c624978e8a0e3198dd38a555cd
Pedro Miguel <pedro.miguel@live.co.uk>
2017-08-03 11:51:36 +0100

    Adds admin view for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'published_date', 'status')
+    list_filter = ('status', 'created', 'published_date', 'author')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'published_date'
+    ordering = ['status', 'published_date']
+
+admin.site.register(Post, PostAdmin)
