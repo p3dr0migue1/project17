328e8346bca22acd93f71484bcec474ab0c09ab0
Pedro Miguel <pedro.miguel@live.co.uk>
2017-08-08 22:07:20 +0100

    Adds RSS Feed functionality to Blog app.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from . import views
+from .feeds import LatestPostsFeed
 
 
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
         views.post_detail,
         name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+    url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
