35ce9ec56064b107cd94e6f354f59ebf6ec1b1f7
Pedro Miguel <pedro.miguel@live.co.uk>
2017-08-09 16:52:45 +0100

    Adds account app

--- project17/settings.py
+++ project17/settings.py
@@ -37,10 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'haystack',
+    'blog',
+    'account'
 ]
 
 MIDDLEWARE_CLASSES = [
